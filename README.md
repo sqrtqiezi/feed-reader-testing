e358039c1ff99600e5b1d9c93d8aa57edc0e140d
sqrtqiezi <njin.cool@gmail.com>
2017-11-24 22:57:48 +0800

    doc: 完善注释和文档

--- README.md
+++ README.md
@@ -1,34 +1,20 @@
-# 项目预览
-
-在这个项目中，你会得到一个基于 web 的用来读取 rss 源的应用。最开始的这个项目的开发者意识到了测试的价值，他们也已经把 [Jasmine](http://jasmine.github.io) 包含在了项目之中，而且甚至已经开始写他们第一个测试用例。但是不幸的是，他们决定去创建一个他们自己的公司，所以我们现在拿到的是一个缺乏完整测试用例的应用，这样是你会参与到这个项目的原因。
+前端学位订阅阅读器测试项目
+================
 
 ## 项目目的
+掌握 [Jasmine](https://jasmine.github.io/) 测试框架的基本用法
 
-测试是开发过程中一个很重要的部分，很多组织把一个标准的开发过程称之为测试驱动开发。意思就是开发人员在他们开始着手编写应用代码之前先写好测试用例。当然这个时候所有的测试用例都是通不过的，然后他们就开始编写应用代码使测试全部通过。
+## 评审标准 
+[https://review.udacity.com/#!/projects/3442558598/rubric](https://review.udacity.com/#!/projects/3442558598/rubric)
 
-不管你是在一个推崇测试驱动开发的组织，或者是一个编写测试只是为了防止未来的开发中出现与已有代码冲突的 bug 的团队工作，测试都是我们要掌握的一项重要技能。
-
-## 我会学到什么
-
-你会学到怎么使用 Jasmine 来给已经写好的应用编写一定数量的测用例。这些测试用例既要测试深层次的商业逻辑，也要测试时间处理和 DOM 操作。
-
-## 这对我的职业有何帮助？
-
-* 编写测试需要分析应用中诸如 html , css , javascript 之类的各个层面。当你换了团队工作或者加入到一个新的公司，这项技能尤其重要（译者注：指阅读新的项目代码的能力）
-* 长期编写测试会让你拥有不需要手动编写测试去测试所有的功能就能快速分析新的代码是否和已知代码冲突的能力。
-
-
-# 我如何完成这个项目
-
-查看订阅阅读器项目的[评审标准](https://review.udacity.com/#!/projects/3442558598/rubric)
-
+## 我如何完成这个项目
 1. 上一下 javascript Testing [课程](https://www.udacity.com/course/ud549)
-2. 下载[必要的项目资源](http://github.com/udacity/frontend-nanodegree-feedreader))
+2. 下载[必要的项目资源](http://github.com/udacity/frontend-nanodegree-feedreader)
 3. 在浏览器里面查看一下应用的功能
 4. 查看项目的 HTMl (**./index.html**), CSS (**./css/style.css**) 和 JavaScript (**./js/app.js**) 文件来对项目的工作原理有一个基本的了解。
 5. 查看 Jasmine spec 文件 **./jasmine/spec/feedreader.js** 然后翻阅阅读 [Jasmine 文档](http://jasmine.github.io)。
 6. 编辑 **./js/app.js** 里面的 `allFeeds` 变量使给出的测试通不过，然后观察Jasmine是怎么展示你应用的错误信息的。
-7. 将 `allFeeds` 变量返回给一个短暂的状态。（待修改）
+7. 将 `allFeeds` 变量返回给一个短暂的状态。
 8. 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
 9. 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
 10. 写一个叫做 `"The menu"` 的测试用例
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,29 +1,10 @@
-/* feedreader.js
- *
- * 这是 Jasmine 会读取的spec文件，它包含所有的要在你应用上面运行的测试。
- */
-
-/* 我们把所有的测试都放在了 $() 函数里面。因为有些测试需要 DOM 元素。
- * 我们得保证在 DOM 准备好之前他们不会被运行。
- */
 $(function () {
-  /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
-   * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
-  */
   describe('RSS Feeds', function () {
-    /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
-     * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
-     * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
-     * 页面看看会发生什么。
-    */
     it('are defined', function () {
       expect(allFeeds).toBeDefined()
       expect(allFeeds.length).not.toBe(0)
     })
 
-    /*
-     * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
-     */
     it('everyone has valid link', function () {
       allFeeds.forEach(function (item) {
         expect(item.url).toBeDefined()
@@ -31,9 +12,6 @@ $(function () {
       })
     })
 
-    /*
-     * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
-     */
     it('everyone has valid name', function () {
       allFeeds.forEach(function (item) {
         expect(item.name).toBeDefined()
@@ -49,20 +27,15 @@ $(function () {
       $trigger = $('.menu-icon-link')
       $body = $('body')
     })
-    /*
-     * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
-     * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
+
+    /**
+     * body 上是否有 class menu-hidden，为菜单是否隐藏的判断条件
      */
     it('are invisibled', function () {
       expect($('body').attr('class')).toContain('menu-hidden')
     })
 
-    /*
-     * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
-     * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
-     * 再次点击的时候是否隐藏。
-     */
-    it('should toggle class show when fired', function () {
+    it('should toggle class show when click event fired', function () {
       $trigger.trigger('click')
       expect($body.attr('class')).not.toContain('menu-hidden')
       $trigger.trigger('click')
@@ -71,22 +44,14 @@ $(function () {
   })
 
   describe('Initial Entries', function () {
-
     beforeEach(function (done) {
+      // 定义 spy ，帮助测试 loadFeed 调用情况
       spyOn(window, 'loadFeed').and.callThrough()
-
       loadFeed(0, function () {
         done()
       })
     })
 
-    /*
-     * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
-     * 里面至少有一个 .entry 的元素。
-     *
-     * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
-     * 和异步的 done() 函数。
-     */
     it('has entries', function (done) {
       expect(loadFeed).toHaveBeenCalled()
       expect($('.feed .entry').length).toBeGreaterThan(0)
@@ -99,18 +64,14 @@ $(function () {
     
     beforeEach(function (done) {
       originalArticleTitle = $('.feed .entry h2').eq(0).text()
-
       // 加载新的 Feed
       loadFeed(1, function () {
         done()
       })
     })
 
-    /*
-     * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
-     * 记住，loadFeed() 函数是异步的。
-     */
     it('entries changed', function (done) {
+      // 判断第一条 article 标题是否变化
       expect($('.feed .entry h2').eq(0).text()).not.toBe(originalArticleTitle)
       done()
     })
